9f96906cf536a5139595710a6bd7bd8b8b072e31
ridesnstuffgt@gmail.com <alejandrogallardo>
2021-11-27 10:49:17 -0600

    Creacion y uso de logs

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -4,6 +4,9 @@ import com.fundamentos.springboot.fundamentos.bean.MyBean;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,16 +15,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 
+	private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
+
 	private ComponentDependency componentDependency;
     private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
 	private MyBeanWithProperties myBeanWithProperties;
+	private UserPojo userPojo;
 
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties) {
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo) {
 		this.componentDependency = componentDependency;
         this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myBeanWithProperties = myBeanWithProperties;
+		this.userPojo = userPojo;
 	}
 
 	public static void main(String[] args) {
@@ -34,5 +41,12 @@ public class FundamentosApplication implements CommandLineRunner {
         myBean.print();
 		myBeanWithDependency.printWithDependendy();
 		System.out.println( myBeanWithProperties.function() );
+		System.out.println(userPojo.getEmail() + " - " + userPojo.getPassword());
+		try {
+			int division = 10 / 0;
+			LOGGER.debug("Mi valor es: " + division);
+		} catch (Exception e) {
+			LOGGER.error("ESTO ES UN ERROR DEL APLICATIVO: " + e.getMessage());
+		}
 	}
 }
--- src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
@@ -1,6 +1,11 @@
 package com.fundamentos.springboot.fundamentos.bean;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class MyBeanWithDependencyImplement implements MyBeanWithDependency {
+
+    Log LOGGER = LogFactory.getLog(MyBeanWithDependencyImplement.class);
 
     private  MyOperation myOperation;
 
@@ -10,7 +15,9 @@ public class MyBeanWithDependencyImplement implements MyBeanWithDependency {
 
     @Override
     public void printWithDependendy() {
+        LOGGER.info("Hemos ingresado al metoo print");
         int numero = 1;
+        LOGGER.debug("El numero enfia es: " + numero);
         System.out.println(myOperation.sum(numero));
         System.out.println("Hola deade la implementacion de un bean con dependencia");
     }
