6d6c3bf901e41205a0756c6af4ec318516da93df
ridesnstuffgt@gmail.com <alejandrogallardo>
2021-11-27 14:40:26 -0600

    Registro en base de datos con JpaRepository

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -4,7 +4,9 @@ import com.fundamentos.springboot.fundamentos.bean.MyBean;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentos.springboot.fundamentos.entity.User;
 import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
+import com.fundamentos.springboot.fundamentos.repository.UserRepository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -12,23 +14,30 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 
 	private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
 
+//	Dependencia para inyectar
 	private ComponentDependency componentDependency;
     private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
 	private MyBeanWithProperties myBeanWithProperties;
 	private UserPojo userPojo;
+	private UserRepository userRepository;
 
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo) {
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo, UserRepository userRepository) {
 		this.componentDependency = componentDependency;
         this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myBeanWithProperties = myBeanWithProperties;
 		this.userPojo = userPojo;
+		this.userRepository = userRepository;
 	}
 
 	public static void main(String[] args) {
@@ -37,8 +46,22 @@ public class FundamentosApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+//		ejemplosAnteriones();
+		saveUserInDataBase();
+	}
+
+	private void saveUserInDataBase() {
+		User user1 = new User("Alex", "alex@me.com", LocalDate.of(2021, 06, 05));
+		User user2 = new User("Juan", "juan@me.com", LocalDate.of(2022, 06, 05));
+
+		List<User> list = Arrays.asList(user1, user2);
+//		se registra en base de dats
+		list.stream().forEach(userRepository::save);
+	}
+
+	private void ejemplosAnteriones() {
 		componentDependency.saludar();
-        myBean.print();
+		myBean.print();
 		myBeanWithDependency.printWithDependendy();
 		System.out.println( myBeanWithProperties.function() );
 		System.out.println(userPojo.getEmail() + " - " + userPojo.getPassword());
